8a68d97927b7eff45f39b33fedb5d372d2bea54b
Josh Lemer <joshlemer@gmail.com>
2014-08-24 11:16:53 -0500

    In working order, fixed issues with caps and also with the 'over 1.0 issue'

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ import operator
 
 class User:
     def __init__(self,projection, user):
-        self.userName = str(user)
+        self.userName = str(user.name)
         self.subredditFrequencies = {}
         self.userObject = user
         self.projection = projection
@@ -12,29 +12,33 @@ class User:
     def get_frequencies(self):
         print "Processing User " + self.userName
         for comment in self.userObject.get_comments(limit=self.projection.thing_limit):
-            self.register_comment(comment.subreddit.display_name)
+            self.register_comment(str(comment.subreddit.display_name.strip(' ').lower()))
 
     def register_comment(self, subredditName):
-        if subredditName in self.subredditFrequencies:
-            self.subredditFrequencies[subredditName] += 1
+        if str(subredditName.strip(' ').lower()) in self.subredditFrequencies:
+            self.subredditFrequencies[str(subredditName.strip(' ').lower())] += 1
         else:
-            self.subredditFrequencies[subredditName] = 1
+            self.subredditFrequencies[str(subredditName.strip(' ').lower())] = 1
 
-        if subredditName is self.projection.subreddit.display_name:
+        if str(subredditName.strip(' ').lower()) is str(self.projection.subreddit.display_name.strip(' ').lower()):
             self.projection.totalComments += 1
-            """
-            print subredditName
-            print self.subredditFrequencies
-            """
 
         self.totalComments += 1
+        """
+        print "THIS IS REGISTERING"
+        print subredditName.strip(' ').lower() + " " + self.projection.subreddit.display_name.strip(' ').lower()
+        print subredditName
+        print self.subredditFrequencies
+        else:
+        print subredditName.strip(' ').lower() + " " + self.projection.subreddit.display_name.strip(' ').lower()
+        """
 
 
 class Projection:
     def __init__(self, subredditName):
         user_agent = ("Testing Reddit Functionality by /u/Nomopomo https://github.com/joshlemer/RedditProject")
         self.reddit = praw.Reddit(user_agent)
-        self.thing_limit = 1000
+        self.thing_limit = 100
         self.subreddit = self.reddit.get_subreddit(subredditName)
         self.comments = {}
         self.subredditFrequencies = {}
@@ -60,7 +64,11 @@ class Projection:
     def calculate_frequency(self, commentor, subreddit):
         #(commentorComments in origSub / totalInOriginalSub) * (frequencyOfCommentorInSubreddit / totalCommenterComments)
 
-        origSubComments = commentor.subredditFrequencies.get(self.subreddit.display_name)
+        #origSubComments = commentor.subredditFrequencies.get(str(self.subreddit.display_name).strip(' ').lower())
+        origSubComments = 0
+        for comment in self.comments:
+            if str(commentor.userName) == str(comment.author.name):
+                origSubComments += 1
 
         if origSubComments is None:
             origSubComments = 0.0
@@ -68,7 +76,9 @@ class Projection:
         if absFreq is None:
             absFreq = 0.0
 
-        return ((origSubComments + 0.0)/len(self.comments)) * ((0.0 + absFreq) / commentor.totalComments)
+        result = ((origSubComments + 0.0)/len(self.comments)) * ((0.0 + absFreq) / commentor.totalComments)
+        print "%s %s %s %s %s" % (origSubComments, len(self.comments), absFreq, commentor.totalComments, result)
+        return result
 
     def get_commentor_frequencies(self):
         for comment in self.comments:
@@ -78,26 +88,33 @@ class Projection:
                 newUser.get_frequencies()
                 self.commentors.append(newUser)
 
-myProj = Projection('Anarcho_Capitalism')
+def run_Analysis():
+    #theSubreddit = 'Anarcho_CapiTALism'
 
-myProj.get_comments()
+    myProj = Projection('Bonsai')
 
-myProj.get_commentor_frequencies()
+    myProj.get_comments()
 
-myProj.register_subreddit_frequencies()
+    myProj.get_commentor_frequencies()
 
-myList = []
-for key, value in myProj.subredditFrequencies.iteritems():
-    temp = [key, value]
-    myList.append(temp)
+    myProj.register_subreddit_frequencies()
 
-myList = sorted(myList, key=operator.itemgetter(1),reverse=True)
+    myList = []
+    mySum = 0.0
+    for key, value in myProj.subredditFrequencies.iteritems():
+        temp = [key, value]
+        mySum += value
+        myList.append(temp)
 
-for item in myList:
-    print "%s   %s" % (item[0], item[1])
+    myList = sorted(myList, key=operator.itemgetter(1),reverse=True)
+
+    for item in myList:
+        print "%s   %s" % (item[0], item[1])
+
+    print mySum
 
 
-
+run_Analysis()
 #print myProj.subredditFrequencies
 #print sorted(myProj.subredditFrequencies.iteritems(), key=operator.itemgetter(1))
 
